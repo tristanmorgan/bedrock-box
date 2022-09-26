d71d9002ae8ba62471bf30bc77978dcb5d9c9166
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2022-09-27 09:18:00 +1000

    Push after build in script.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker build --pull --no-cache --build-arg mcversion=$1 -t tristanmorgan/bedrock-box:$1 -t tristanmorgan/bedrock-box:latest .
-
+docker push tristanmorgan/bedrock-box:$1
+docker push tristanmorgan/bedrock-box:latest
